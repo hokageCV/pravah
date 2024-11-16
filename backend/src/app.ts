c3e4ca74e8657f14dbd3e4cc508b9e2127c373aa
hokageCV <chaitanyavaru@gmail.com>
2024-11-16 22:49:37 +0530

    Move env validation to middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,9 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { PinoLogger } from 'hono-pino';
 import { notFound } from 'stoker/middlewares';
+import { EnvType } from './env';
 import pinoLogger from './middlewares/logger';
-import { validateEnv, EnvType } from './env';
+import ValidateEnv from './middlewares/validate-env';
 
 type AppBindings = {
   Bindings: EnvType,
@@ -13,11 +14,11 @@ type AppBindings = {
 
 const app = new OpenAPIHono<AppBindings>()
 
+app.use('*', ValidateEnv)
 app.use(pinoLogger)
 
 app.get('/', (c) => {
-  const env = validateEnv(c.env)
-  return c.text(`test key: ${env.APIKEY}`);
+  return c.text(`test key: ${c.env.APIKEY}`);
 })
 
 app.notFound(notFound)
---
+++ backend/src/middlewares/validate-env.ts
@@ -0,0 +1,9 @@
+import { validateEnv } from '@/env';
+import { createMiddleware } from 'hono/factory';
+
+const ValidateEnv = createMiddleware(async (c, next) => {
+  c.env = validateEnv(c.env)
+  await next()
+})
+
+export default ValidateEnv;

7e119cd1714e89d6db3b7c9c2e4cb7e779039d46
hokageCV <chaitanyavaru@gmail.com>
2025-05-05 19:46:41 +0530

    Persist auth store

--- frontend/src/components/auth/auth.store.ts
+++ frontend/src/components/auth/auth.store.ts
@@ -1,11 +1,20 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 type AuthState = {
   user: { id: number; username: string } | null;
-  setUser: (user: AuthState["user"]) => void;
+  setUser: (user: AuthState['user']) => void;
 };
 
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-}));
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+    }),
+    {
+      name: 'pravah-auth-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);

551e8704e47818076684eed8c99bde5553116ba7
hokageCV <chaitanyavaru@gmail.com>
2025-06-18 20:25:43 +0530

    Add group action btns into ui

--- frontend/src/components/groups/group-log-graph.tsx
+++ frontend/src/components/groups/group-log-graph.tsx
@@ -4,6 +4,8 @@ import { getMonthData, mapLogToLevels } from '../../utils/date'
 import { capitalize } from '../../utils/text'
 import { DayCell } from '../logs/graph-day-cell'
 import { fetchGroupLogs } from '../logs/log.api'
+import { DeleteSvg2 } from '../svgs/delete2'
+import { EditSvg2 } from '../svgs/edit2'
 
 type GroupLogGraphProps = {
   groupId: number
@@ -38,20 +40,34 @@ export function GroupLogGraph({ groupId, usersData }: GroupLogGraphProps) {
       {usersData.map((userData, idx) => (
         <div
           key={idx}
-          className='h-64 p-2  grid grid-rows-[auto_1fr] items-start justify-center bg-c-surface rounded-md shadow-md'
+          className='min-h-64 p-2 grid grid-rows-[auto,1fr] grid-cols-[1fr,auto] bg-c-surface rounded-md shadow-md gap-2'
         >
-          <h2 className='text-xl font-semibold'>{capitalize(userData.userName)}</h2>
-          <p className='text-c-text-muted'>{capitalize(userData.habitName)}</p>
-          <div className='grid grid-cols-7 gap-1 text-sm content-start'>
-            {monthData.days.map((dayObj, i) => (
-              <DayCell
-                dayObj={dayObj}
-                logGoalLevels={logGoalLevels}
-                index={i}
-                habitId={userData.habitId}
-                habitCreatedAt={userData.habitCreatedAt}
-              />
-            ))}
+          <div className='col-start-1 row-start-1 overflow-hidden'>
+            <h2 className='text-xl font-semibold truncate'>{capitalize(userData.userName)}</h2>
+            <p className='text-c-text-muted truncate'>{capitalize(userData.habitName)}</p>
+          </div>
+
+          <div className='col-start-2 row-start-1 flex gap-2 shrink-0 justify-self-end self-start'>
+            <button className='p-1 text-c-text hover:text-c-primary'>
+              <EditSvg2 />
+            </button>
+            <button className='p-1 text-c-text hover:text-c-danger'>
+              <DeleteSvg2 />
+            </button>
+          </div>
+
+          <div className='col-span-2 row-start-2'>
+            <div className='grid grid-cols-7 gap-1 text-sm max-w-cell-w max-h-cell-h m-auto'>
+              {monthData.days.map((dayObj, i) => (
+                <DayCell
+                  dayObj={dayObj}
+                  logGoalLevels={logGoalLevels}
+                  index={i}
+                  habitId={userData.habitId}
+                  habitCreatedAt={userData.habitCreatedAt}
+                />
+              ))}
+            </div>
           </div>
         </div>
       ))}
